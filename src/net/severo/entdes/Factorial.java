2d170fd4aea69c7bc1d31a2638f801ec234e594e
Bufon <edelacotera@gmail.com>
2023-01-29 20:10:35 +0100

    corrección de n

--- src/net/severo/entdes/Factorial.java
+++ src/net/severo/entdes/Factorial.java
@@ -6,15 +6,18 @@ package net.severo.entdes;
  */
 public class Factorial {
 
+	public static final java.lang.String VALUE_OF_N = "Arvo n!: ";
+	public static final java.lang.String VALUE_OF_N1 = "Arvo n: ";
+
 	public static void main(String[] args) {
 
-		int n = (int) (Math.random() * 10);
+		int n = (int) (Math.random() * 1);
 		int result = 1;
-		System.out.println("Value of n: " + n);
+		System.out.println(VALUE_OF_N1 + n);
 		while (n > 1) {
 			result = result * (n - 1);
 		}
-		System.out.println("Value of n!: " + result);
+		System.out.println(VALUE_OF_N + result);
 
 	}
 
